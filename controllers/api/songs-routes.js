7deab8040271d3327727821f7ca4a2504de1067a
ThomasCerr <99366289+ThomasCerr@users.noreply.github.com>
2022-06-02 20:43:43 -0400

    initial models and basic routes set up

---
+++ controllers/api/songs-routes.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { User, Songs } = require('../../models');
+
+router.get('/', (req, res) => {
+    Songs.findAll({
+        attributes: [
+            'id',
+            'song_name',
+            'search_url',
+            'user_id'
+        ],
+        include: [
+            {
+                model:User,
+                attributes: ['username'],
+            }
+        ]
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  router.post('/', (req, res) => {
+    // expects {song_name: 'My Way', song_name: 'https://taskmaster.com/press', user_id: 1}
+    if (req.session) {
+      Post.create({
+        song_name: req.body.song_name,
+        search_url: req.body.search_url,
+        user_id: req.session.user_id
+      })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    }
+  });
+
+  module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, Songs } = require('../models');
+
+module.exports = router;
